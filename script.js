fbaccf732e59adab5fd6178eaa2ef8461a6592a9
Tamuna-Z <tamunazurabashvili3@gmail.com>
2025-09-03 22:30:40 +0400

    add js varibales

---
+++ script.js
@@ -0,0 +1,19 @@
+
+// string type
+var userName = "Nina"
+console.log(userName);
+// number
+var age = 12;
+console.log(age);
+
+// boolean true/ false
+var is_student = true;
+console.log(is_student);
+
+// null
+
+var point = null;
+
+// undefined
+
+var salary = undefined;
