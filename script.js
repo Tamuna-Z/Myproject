c54bf4f49e3c997e6872823c1fdb5c802bce3ccd
TAMARI <90831707+Tamuna-Z@users.noreply.github.com>
2025-09-05 22:01:46 +0400

    add lecture _js1

--- script.js
+++ script.js
@@ -1,14 +1,146 @@
 // variables var, let const
+var userName = "Gio"
+var age = 12;
+
 // ცვლადის წერის სინტაქსი
 // declaration, assignment, reassignment,hoisting
+// var data;
+// data = "hello"
+// var box = "world"
+// console.log(box)
+// var box = "bye"
+// console.log(box)
+
+
+// let box ="toy1"
+//  box = "toy2"
+//  console.log(box)
+
+// const data = "hello"
+
+
+
 // მონაცემთა ტიპები,პრიმიტიული, რეფერენსი
+
 // არითმეტიკული ოპერატორები +,-,/, %, **, ++, --
+// var number1 = 23;
+// var number2 = 2;
+
+// console.log(number1 + number2)
+// console.log(number1 - number2)
+// console.log(number1 * number2)
+// console.log(number1 / number2)
+
+// var number1 = '23';
+// var number2 = '2';
+
+// console.log(number1 + number2)
+// console.log(number1 - number2)
+// console.log(number1 * number2)
+// console.log(number1 / number2)
 // შედარების ოპერატორები == , ===, =, !== , > , <, >= , <= ,
+let data = 5 ; //მინიჭების 
+5 == 5       //true
+5 =='5'     //true
+
+5 === 5       //true
+5 ==='5'     //false
+
+5 !== 5       //false
+5 !=='5'     //true
+5>4
+3<2
+
+
+
+
+
 // ლოგიკური ოპერატორები  && , ||
+
+
 // Ternary ოპერატორი  ?:
+
 // პირობითი ოპერატორები if, else if, else, switch
+// let temp = 25;
+
+// if(temp > 28){
+//     console.log("ცხელა")
+// } else if(temp < 25){
+//     console.log("თბილა")
+// }else {
+//     console.log("ცივა")
+// }
+
+
+// if(){
+
+// } else if(){
+
+// }else if (){
+
+// }else{
+
+// }
 // კოერცია, იმპლისიტური , ექსპლისიტური 
 // prompt მეთოდი
+// let answer = Number(prompt("რა ღირს"))
+// console.log(answer)
+// let answer1 = Number(prompt("რა ღირს"))
+// console.log(answer1)
 
+// let result = answer + answer1
+// console.log(result)
+
+
+
+
+
+// მასივი, array მეთოდები
+        //  0   1    2      3
+let data1 = [2, 45,"hello", true];
+data1.push(50) //ბოლოში ამატებს
+console.log(data1)
+data1.unshift(23)// ამატებს დასაწყისში
+console.log(data1)
+data1.pop()  //შლის ბოლო ელემენტს
+console.log(data1)
+data1.shift()
+console.log(data1) // შლის საწყის ელემენტს
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  let day = "saturday";
+// switch(day) {
+//   case "monday": console.log("work"); break;
+//   case "tuesday": console.log(" part time "); break;
+//   case "thursday": console.log(" full time "); break;
+//   case "friday": console.log(" day off "); break;
+//   case "saturday": console.log(" party time "); break;
+//   case "sunday": console.log(" holiday "); break;
+//   default: console.log("");
+// }
 
 
